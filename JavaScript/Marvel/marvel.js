ec4376c2d0375dd1e0b0312d879472190a2b97e9
NiPZav <103854872+NiPZav@users.noreply.github.com>
2023-11-08 23:31:18 -0700

    Practica JS - Actualizada y terminada

--- JavaScript/Marvel/marvel.js
+++ JavaScript/Marvel/marvel.js
@@ -1,13 +1,14 @@
 var numero = 0;
 var cantidad = 0;
+var personaje = "";
 var datos = [];
 
 async function apimarvel() {
     numero = 0;
 
-    cantidad = document.getElementById("cantidad").value;
+    personaje = document.getElementById("personaje").value;
 
-    const respuesta = await fetch("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c");
+    const respuesta = await fetch("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="+personaje);
     datos = await respuesta.json();
 
     caja();
@@ -30,14 +31,16 @@ function siguiente() {
 }
 
 function caja() {
-    console.log("Arreglo: "+numero);
-
     let foto = document.getElementById("foto");
     let nombre = document.getElementById("nombre");
     let descripcion = document.getElementById("descripcion");
     let comicsDisp = document.getElementById("comicsDisponibles");
 
-    //foto.setAttribute("src", datos.data.results.)
+    cantidad = datos.data.total;
+    console.log(cantidad);
+    console.log("Arreglo posicion: "+numero);
+
+    foto.setAttribute("src", datos.data.results[numero].thumbnail.path+"."+datos.data.results[numero].thumbnail.extension);
     nombre.innerHTML = datos.data.results[numero].name;
     descripcion.innerHTML = datos.data.results[numero].description;
     comicsDisp.innerHTML = datos.data.results[numero].comics.available;
